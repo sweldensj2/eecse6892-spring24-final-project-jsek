d67113eac78db53a090adec78adcdb602b08bed2
sweldensj2 <jws2215@columbia.edu>
2024-04-22 18:31:36 -0400

    dqn 1/4 working

--- .ipynb_checkpoints/DQN_Model_improved-checkpoint.py
+++ .ipynb_checkpoints/DQN_Model_improved-checkpoint.py
@@ -5,26 +5,22 @@ import torch.nn.functional as F
 class DQN_improved(nn.Module):
     def __init__(self, num_inputs, num_outputs):
         super(DQN_improved, self).__init__()
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
         
-        # Define the neural network architecture
-        self.model = nn.Sequential(
-            nn.Linear(num_inputs, 256),  # Input layer
-            nn.ReLU(),                    # Activation function
-            nn.BatchNorm1d(256),          # Batch normalization
-            nn.Dropout(0.5),              # Dropout for regularization
-            
-            nn.Linear(256, 512),          # Hidden layer
-            nn.ReLU(),                    # Activation function
-            nn.BatchNorm1d(512),          # Batch normalization
-            nn.Dropout(0.5),              # Dropout for regularization
-            
-            nn.Linear(512, 256),          # Hidden layer
-            nn.ReLU(),                    # Activation function
-            nn.BatchNorm1d(256),          # Batch normalization
-            nn.Dropout(0.5),              # Dropout for regularization
-            
-            nn.Linear(256, num_outputs)   # Output layer
-        )
+        
+        self.fc1 = nn.Linear(num_inputs, 512)
+        self.dropout1 = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(512, 256)
+        self.dropout2 = nn.Dropout(p=0.5)
+        self.fc3 = nn.Linear(256, 128)
+        self.dropout3 = nn.Dropout(p=0.5)
+        self.fc4 = nn.Linear(128, num_outputs)
+
+        # Dropout layers
+        
+        
+        
         
         # Initialize the weights using Xavier initialization
         for m in self.modules():
@@ -33,9 +29,19 @@ class DQN_improved(nn.Module):
         
     def forward(self, x):
         # Forward pass through the network
-        return self.model(x)
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
+        x = F.relu(self.fc3(x))
+        x = self.dropout3(x)
+        x = self.fc4(x)
+
+        return x
+        
     
     def get_action(self, input):
-        qvalues = self.forward(input)
-        action = torch.argmax(qvalues, dim=1)
+        qvalue = self.forward(input)
+        action = torch.argmax(qvalue)
         return action.cpu().numpy()
