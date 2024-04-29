651f46ea9fcfd4250bfc70eb0a89bd681a7e9038
Johan Sweldens <jws2215@columbia.edu>
2024-04-29 15:56:46 +0000

    breakout mlp doesn't work

---
+++ .ipynb_checkpoints/DQN_Model_CNN-checkpoint.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DQN_CNN(nn.Module):
+    def __init__(self, input_shape, num_outputs):
+        super(DQN_CNN, self).__init__()
+        self.num_outputs = num_outputs
+
+        # Define the convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=input_shape[2], out_channels=32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+
+        # Define the fully connected layers
+        self.fc1 = nn.Linear(in_features=622528, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=num_outputs)
+
+    def forward(self, x):
+        # reshape x from the state [210, 160, 1] --> [1, 210, 160]
+        x = x.permute(0, 3, 1, 2)
+        
+        # Convolutional layers
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        # Flatten the output of the convolutional layers
+        x = x.view(x.size(0), -1)
+
+        # Fully connected layers
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def get_action(self, input):
+        qvalues = self.forward(input)
+        action = torch.argmax(qvalues, dim=1)
+        return action.cpu().numpy()
