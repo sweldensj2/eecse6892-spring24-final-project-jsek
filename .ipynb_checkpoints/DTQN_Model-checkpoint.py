eee3d9721f8f2d800a76cbe9a705afe0cd781aac
sweldensj2 <jws2215@columbia.edu>
2024-04-30 16:36:47 -0400

    removing CNN efforts, trained on DQN paper parameters

---
+++ .ipynb_checkpoints/DTQN_Model-checkpoint.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DTQN(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(DTQN, self).__init__()
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+
+        self.fc = nn.Linear(num_inputs, 64)
+        self.Tlayer = nn.TransformerEncoderLayer(d_model=64, nhead=2)
+        self.transformerE = nn.TransformerEncoder(self.Tlayer, num_layers=3)
+
+        self.fc1 = nn.Linear(64, 32)
+        self.fc2 = nn.Linear(32, num_outputs)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+
+    def forward(self, x):
+        # x = x.transpose(0, 1)
+        x = self.fc(x)
+        out = self.transformerE(x)
+        # out = out.transpose(0, 1)
+        out = F.relu(self.fc1(out))
+        qvalue = self.fc2(out)
+
+        return qvalue
+    
+    def get_action(self, input):
+        input = input.unsqueeze(0)
+        print(input)
+        print(input.shape)
+        qvalue = self.forward(input)
+        action = torch.argmax(qvalue)
+        return action.cpu().numpy()
